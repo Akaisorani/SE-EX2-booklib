731dbddcc98370edf9459e52c46e5f08f25aa6bf
Akaisorani <1224067801@qq.com>
2017-10-15 00:19:22 +0800

    add some comment

--- src/booklib/jdbc_test.java
+++ src/booklib/jdbc_test.java
@@ -4,7 +4,7 @@ public class jdbc_test {
 	public static void main(String args[]) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Success loading Mysql Driver!");
+			System.out.println("Success loading Mysql Driver! ");
 		}
 		catch (Exception e){
 			System.out.println("Error loading Mysql Driver!");
